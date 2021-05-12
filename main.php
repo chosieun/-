2e04d73900254601b2c9338d01c3e272cc115d9f
chosieun <83362005+chosieun@users.noreply.github.com>
2021-05-13 00:43:20 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>조시은 웹사이트</title>
+<head>
+<body>
+<?php header("Content-Type: text/html; charset=UTF-8"); 
+session_start(); ?>
+<?php if( (!isset($_SESSION['id'])) &&  (!isset($_SESSION['nickname'])) ){ ?>
+<a href="join.html">회원가입</a><br>
+<a href="login.html">로그인</a><br>
+<?php } else {?>
+<a href="logout.php">로그아웃</a><br>
+<?php } ?>
+<a href="board.php">게시판</a>
+</body>
+</html>
